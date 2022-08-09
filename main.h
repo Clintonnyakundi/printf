4a90d8ac04ab3b139f36215abc482cfa8bf0d4a0
Clintonnyakundi <mokayaclinton34@gmail.com>
2022-08-09 06:12:43 -0700

    Fix code for the _printf function as in stdio

--- main.h
+++ main.h
@@ -1,47 +1,52 @@
 #ifndef MAIN_H
 #define MAIN_H
-#include <stdarg.h>
-#include <unistd.h>
+
 #include <stdlib.h>
-#include <stdio.h>
+#include <stdarg.h>
 /**
- *struct convert - structure for symbols and functions
- *@sym: operator
- *@f: function
+ * struct flags - struct containing flags to "turn on"
+ * when a flag specifier is passed to _printf()
+ * @plus: flag for the '+' character
+ * @space: flag for the ' ' character
+ * @hash: flag for the '#' character
  */
-struct convert
+typedef struct flags
 {
-	char *sym;
-	int (*f)(va_list);
-};
-typedef struct convert conver_t;
+	int plus;
+	int space;
+	int hash;
+} flags_t;
 
-/*Main functs*/
-int _printf(const char *format, ...);
-int _putchar(char c);
-int print_char(va_list list);
-int print_string(va_list list);
-int print_percent(__attribute__((unused))va_list list);
-int print_integer(va_list list);
-int unsigned_integer(va_list list);
-int parser(const char *format, conver_t funcs[], va_list va);
-int print_address(va_list list);
-
-/*Helper functs*/
-int print_binary(va_list list);
-int print_octal(va_list list);
-int print_hex(va_list list);
-int print_heX(va_list list);
-int hex_check(int num, char x);
-int print_reversed(va_list arg);
-int rot13(va_list list);
-char *rev_string(char *s);
-void write_base(char *str);
-unsigned int base_len(unsigned int num, int base);
-char *_memcpy(char *dest, char *src, unsigned int n);
-int print_number(va_list args);
-int print_unsigned_number(unsigned int n);
-int _puts(char *str);
+/**
+ * struct printHandler - struct to choose the right function depending
+ * on the format specifier passed to _printf()
+ * @c: format specifier
+ * @f: pointer to the correct printing function
+ */
+typedef struct printHandler
+{
+	char c;
+	int (*f)(va_list ap, flags_t *f);
+} ph;
+int print_int(va_list l, flags_t *f);
+void print_number(int n);
+int print_unsigned(va_list l, flags_t *f);
+int count_digit(int i);
+int print_hex(va_list l, flags_t *f);
+int print_hex_big(va_list l, flags_t *f);
+int print_binary(va_list l, flags_t *f);
+int print_octal(va_list l, flags_t *f);
 char *convert(unsigned long int num, int base, int lowercase);
-
+int _printf(const char *format, ...);
+int (*get_print(char s))(va_list, flags_t *);
+int get_flag(char s, flags_t *f);
+int print_string(va_list l, flags_t *f);
+int print_char(va_list l, flags_t *f);
+int _putchar(char c);
+int _puts(char *str);
+int print_rot13(va_list l, flags_t *f);
+int print_rev(va_list l, flags_t *f);
+int print_bigS(va_list l, flags_t *f);
+int print_address(va_list l, flags_t *f);
+int print_percent(va_list l, flags_t *f);
 #endif
